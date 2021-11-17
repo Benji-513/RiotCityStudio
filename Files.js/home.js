2ad6b35a079f7bdcb3e0eedb38829e390f5ce27a
Travis Hacker <travisb081988@gmail.com>
2021-11-17 17:33:57 -0500

    added a hamburger menu with JS and styled it. Also started on card containers for the recording rooms

---
+++ Files.js/home.js
@@ -0,0 +1,9 @@
+/* Hamburger menu toggle links */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
